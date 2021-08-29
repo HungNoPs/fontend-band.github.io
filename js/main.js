c7a87b596f27191134cddc803b54e9ad58f397c6
Hung <xbook2k@gmail.com>
2021-08-29 18:44:24 +0700

    upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
-/* Slide */
+/* Slide Basic */
 var index = 0;
-slide();
-function slide() {
+(function slide() {
   let slideList = document.querySelectorAll(".banner-content");
   slideList.forEach((e) => (e.style.display = "none"));
   index++;
   if (index >= slideList.length) index = 0;
   slideList[index].style.display = "block";
   setTimeout(slide, 4000);
-}
-/* Header */
+})();
+
+/* Header && Menu Mobile */
 const menubar = document.querySelector(".header-menubar");
 const menuMobile = document.querySelector(".menumb");
 const listMenuMobile = document.querySelectorAll(".menumb-link");
-menubar.addEventListener("click", () => {
-  menuMobile.classList.toggle("show");
-});
-listMenuMobile.forEach((e) => {
-  e.addEventListener("click", () => {
-    if (menubar) menuMobile.classList.remove("show");
-  });
-});
+const home = document.querySelector(".menu-link");
+
+// show menubar
+menubar.addEventListener("click", () => menuMobile.classList.toggle("show"));
+
+// remove menubar
+const removeMenu = () => menuMobile.classList.remove("show");
+home.addEventListener("click", removeMenu);
+listMenuMobile.forEach((e) => e.addEventListener("click", removeMenu));
+
 /* Tickets */
 const modal = document.querySelector(".modal");
 const modalContainer = document.querySelector(".modal-container");
@@ -35,14 +37,17 @@ btnTour.forEach((e) => {
     modal.style.background = "rgba(0, 0, 0, 0.7)";
   });
 });
+
+// function remove Tickets
 const tickets = () => {
   modalContainer.classList.remove("active");
   modal.style.zIndex = -2;
   modal.style.background = "transparent";
 };
+
+//remove tickets from
 modalClose.addEventListener("click", tickets);
 modalCloseButton.addEventListener("click", tickets);
 modal.addEventListener("click", tickets);
-modalContainer.addEventListener("click", (e) => {
-  e.stopPropagation(); 
-});
+modalContainer.addEventListener("click", (e) => e.stopPropagation());
+/* ------------------------ */
